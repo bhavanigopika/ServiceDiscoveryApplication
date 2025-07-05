e2771dc9aa02d639c2c0c2e0014a8c2487dc9efc
Bhavani Gopikaramanan <bhavanigopika@gmail.com>
2025-07-05 23:56:01 +0530

    eureka server or service discovery - understanding spring cloud

--- src/main/java/com/example/servicediscoveryapplication/ServiceDiscoveryApplication.java
+++ src/main/java/com/example/servicediscoveryapplication/ServiceDiscoveryApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceDiscoveryApplication {
-
+      /*Spring Cloud helps more here like send the request in balanced way see, here we can use eureka server and use @loadbalanced
+      we can do the api gateway (hitting the request from client), calling from one microservice to another microservice easier
+      by enabling eureka server in pom.xml and added dependency for eureka server, load balanced, api gateway dependency*/
     public static void main(String[] args) {
         SpringApplication.run(ServiceDiscoveryApplication.class, args);
     }
